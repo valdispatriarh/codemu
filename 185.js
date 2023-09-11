59929e59c4439d60562e2dfac25694ce13b2138b
Vlad <valdispatriarh@gmail.com>
2023-09-11 18:40:27 +0200

    fix: change task 185

--- 185.js
+++ 185.js
@@ -5,7 +5,10 @@ console.log(Math.round(num), +num.toFixed(1), +num.toFixed(2))
 let num2 = Math.sqrt(587)
 let ceil = Math.ceil(num2)
 let floor = Math.floor(num2)
-let obj185 = {}
+let obj185 = {
+    'ceil': undefined,
+    'floor': undefined
+}
 obj185.ceil = ceil
 obj185.floor = floor
 console.log(num2, obj185)
