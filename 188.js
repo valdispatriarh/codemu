105172e9f5e76d2f1ef17d38509c01f265c6ff01
Vlad <valdispatriarh@gmail.com>
2023-09-11 22:38:35 +0200

    feat: add task 186-190

---
+++ 188.js
@@ -0,0 +1,11 @@
+//https://code.mu/ru/javascript/book/prime/inbuilt/math/abs/
+let a = 2
+let b = 3
+let total188 = Math.abs(a - b)
+console.log('a', a, 'b', b, 'total', total188)
+let arrA = [-8, 8, 88]
+let arrB = [5, 99, 698]
+for(let i = 0; i < 3; i++){
+    total188 = Math.abs(arrA[i] - arrB[i])
+    console.log('a', arrA[i], 'b', arrB[i], 'total', total188)
+}
