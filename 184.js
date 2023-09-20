293b88f4ed4019e791f6c16cbb6eab19d8c730e0
Vlad <valdispatriarh@gmail.com>
2023-09-20 18:52:16 +0200

    fix: change task 1843

--- 184.js
+++ 184.js
@@ -6,7 +6,7 @@ console.log(2, Math.sqrt(245))
 let arr3 = [4, 2, 5, 19, 13, 0, 10]
 let sum184 = 0
 for(let elem of arr3){
-    sum184 = sum184 + (elem**3)
+    sum184 += (elem**3)
 }
 console.log(3, Math.sqrt(sum184))
 
