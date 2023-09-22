9c30af0e41083ee98c1402917bc89c9abd5eff9a
Vlad <valdispatriarh@gmail.com>
2023-09-22 20:51:23 +0200

    feat: add 197

--- 197.js
+++ 197.js
@@ -1,1 +1,32 @@
 //https://code.mu/ru/javascript/book/prime/inbuilt/array/searching/
+let str197 = [1, 2, 3, 4, 5];
+if(str197.includes(3)){
+    console.log('yes')
+} else {
+    console.log('no')
+}
+
+let str1972 = [1, 2, 3, 4, 5];
+let res = str1972.find(function(elem1972){
+   return elem1972 === 3
+})
+if(res){
+    console.log('yes')
+} else {
+    console.log('no')
+}
+
+let str1973 = [1, 2, 3, 4, 5];
+if(str197.indexOf(3) !== -1){
+    console.log('yes')
+} else {
+    console.log('no')
+}
+
+
+let str1974 = [1, 2, 3, 4, 5];
+if(str1974.lastIndexOf(3) !== -1){
+    console.log('yes')
+} else {
+    console.log('no')
+}
