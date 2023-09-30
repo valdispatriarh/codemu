94ab823ac43dd9b5f64ed192f5970e954e74528a
Vlad <valdispatriarh@gmail.com>
2023-09-30 22:37:43 +0200

    feat: add task 201-205

---
+++ 205.js
@@ -0,0 +1,12 @@
+//https://code.mu/ru/javascript/book/prime/functions/basis/return/
+function showNum205(num205){
+    return num205**3
+}
+let res205 = showNum205(3)
+console.log(res205)
+
+function func205(num2052){
+    return num2052**(1/2)
+}
+let res = func205(3) + func205(4)
+console.log(res)
