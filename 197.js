7a90fd720c11e626ecaf34c4bf74d41460b69704
Vlad <valdispatriarh@gmail.com>
2023-10-06 16:18:20 +0200

    fix: change 197 task

--- 197.js
+++ 197.js
@@ -7,15 +7,10 @@ if(str197.includes(3)){
 }
 
 let str1972 = [1, 2, 3, 4, 5];
-let res = str1972.find(function(elem1972){
-   return elem1972 === 3
-})
-if(res){
-    console.log('yes')
-} else {
-    console.log('no')
-}
+let res = str1972.find(elem => elem === 3) ? 'yes' : 'no'
+console.log(res)
 
+   
 let str1973 = [1, 2, 3, 4, 5];
 if(str197.indexOf(3) !== -1){
     console.log('yes')
