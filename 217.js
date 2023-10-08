403fcca665be8123d966c5cacad587f05928c53a
Vlad <valdispatriarh@gmail.com>
2023-10-08 15:04:56 +0200

    feat: add task 216-220

---
+++ 217.js
@@ -0,0 +1,12 @@
+//https://code.mu/ru/javascript/book/prime/functions/variables/local/
+function func217() {
+	let num217 = 5;
+	return num217;
+}
+//console.log(num217);
+
+function func2172() {
+	let num = 5;
+	return num;
+}
+console.log(func2172());
