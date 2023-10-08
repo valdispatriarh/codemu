5b395f3db75eba812662e84b48179fdc8c2dca4f
Vlad <valdispatriarh@gmail.com>
2023-10-08 20:02:45 +0200

    feat: add task 221-225

---
+++ 221.js
@@ -0,0 +1,4 @@
+//https://code.mu/ru/javascript/book/prime/functions/variables/parameters-names-coincidence/
+//1//1
+//2//2
+//3//1
---
+++ 223.js
@@ -0,0 +1,6 @@
+//https://code.mu/ru/javascript/book/prime/functions/types/function-code-result/
+function func223() {
+	return '!';
+}
+document.writeln(func223)
+
