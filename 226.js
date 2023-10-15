884681ac01def6ddb0d5002bc00cac31bbdf63ba
Vlad <valdispatriarh@gmail.com>
2023-10-15 18:47:24 +0200

    feat: add task 226-230

---
+++ 226.js
@@ -0,0 +1,8 @@
+//https://code.mu/ru/javascript/book/prime/functions/types/function-variable-assigning/
+let func2261 = function(){
+    return 1
+}
+let func2262 = function(){
+    return 2
+}
+console.log(func2261()+func2262())
