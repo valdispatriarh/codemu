477738fa8cb82d38be334d29f1cb438e2cb905e3
Vlad <valdispatriarh@gmail.com>
2023-11-14 12:11:54 +0100

    fix: change task 261

--- 261.js
+++ 261.js
@@ -1,11 +1,13 @@
 //https://code.mu/ru/javascript/book/prime/functions/closures/counter/
 function func261(){
     let num = 2
-    return () => {num--
-                 if(num < 0){
-                    return 'отсчет окончен'
-                 }
-                 return num}
+    return () => { 
+        num--
+        if (num < 0) {
+            return 'отсчет окончен'
+        }
+        return num
+    }
 }
 let counter261 = func261()
 console.log(counter261())
